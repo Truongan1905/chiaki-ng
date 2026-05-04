b954e763824049a3bb9bb31a5bd67dacc1d61154
Truongan1905 <truonganlamma@gmail.com>
2026-05-04 11:00:17 +0700

    Fix CMake path to use android/app/CMakeLists.txt

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,8 +50,8 @@ android {
 
     externalNativeBuild {
         cmake {
-            // Use chiaki-ng submodule root CMakeLists.txt (relative path)
-            path = file("${project.projectDir}/CMakeLists.txt")
+            // Use chiaki-ng android/app CMakeLists.txt (relative path)
+            path = file("${project.projectDir}/android/app/CMakeLists.txt")
             version = "3.22.1"
         }
     }
